6ce882b732fa3202e34e7620b59668088525e677
Artur Fogiel <arturprojekty1988@gmail.com>
2019-12-30 21:26:09 +0100

    Rust and Swift ready

--- sort/rust/sort.rs
+++ sort/rust/sort.rs
@@ -27,19 +27,15 @@ impl Sort for SelectionSort {
 
 impl Sort for InsertionSort {
     fn sort(&self, vec: &mut Vec<i32>, low: usize, high: usize) {
-        for i in low..high {
+        let n = high - low;
+        for i in 1..n {
             let key = vec[i];
-            let mut j = 0;
-            if(low > 0) {
-                j = i - 1
+            let mut j: isize = (i as isize - 1) as isize;
+            while j >= 0 && vec[j as usize] > key {
+                vec[(j + 1) as usize] = vec[j as usize];
+                j-=1;
             }
-            while j > 0 && vec[j] > key {
-                vec[j + 1] = vec[j];
-                if j > 0 {
-                    j-=1;
-                }
-            }
-            vec[j + 1] = key;
+            vec[(j + 1) as usize] = key;
         }
     }
 }
@@ -53,6 +49,51 @@ impl Sort for MergeSort {
         let mid: usize = low + (high - low) / 2;
         self.sort(vec, low, mid);
         self.sort(vec, mid + 1, high);
+        self.merge(vec, low, mid, high)
+    }
+}
+
+impl MergeSort {
+    fn merge(&self, vec: &mut Vec<i32>, low: usize, mid: usize, high: usize) {
+        let left_half_size = mid - low + 1;
+        let right_half_size = high - mid;
+
+        let mut i = 0;
+        let mut j = 0;
+        let mut k = low;
+
+        let mut left: Vec<i32> = Vec::new();
+        let mut right: Vec<i32> = Vec::new();
+
+        for i in 0..left_half_size {
+            left.push(vec[low + i]);
+        }
+        for j in 0..right_half_size {
+            right.push(vec[mid + j + 1]);
+        }
+
+        while i < left_half_size && j < right_half_size {
+            if left[i] <= right[j] {
+                vec[k] = left[i];
+                i+=1;
+            } else {
+                vec[k] = right[j];
+                j+=1;
+            }
+            k+=1;
+        }
+
+        while i < left_half_size {
+            vec[k] = left[i];
+            i+=1;
+            k+=1;
+        }
+
+        while j < right_half_size {
+            vec[k] = right[j];
+            j+=1;
+            k+=1;
+        }
     }
 }
 
@@ -62,9 +103,44 @@ impl Sort for QuickSort {
             return
         }
 
-        let part: usize = 0;
+        let part: usize = self.partition(vec, low, high);
         self.sort(vec, low, part - 1);
         self.sort(vec, part + 1, high);
+    }
+}
+
+impl QuickSort {
+    fn partition(&self, vec: &mut Vec<i32>, low: usize, high: usize) -> usize {
+        let mut i = low;
+        let mut j: isize = high as isize + 1;
+        let tmp = vec[low];
+        loop {
+            i+=1;
+            while vec[i] < tmp {
+                i+=1;
+                if i >= high {
+                    break;
+                }                
+            }
+            j-=1;
+            while tmp < vec[j as usize] {
+                j-=1;
+                if j as usize <= low {
+                    break;
+                }
+            }
+            if i>=j as usize {
+                break;
+            }
+            //
+            let tmp = vec[i];
+            vec[i] = vec[j as usize];
+            vec[j as usize] = tmp;
+        }
+        let tmp = vec[low];
+        vec[low] = vec[j as usize];
+        vec[j as usize] = tmp;
+        return j as usize;
     }
 }
 
@@ -81,7 +157,7 @@ fn gen_random_data(size: i32) -> Vec<i32> {
         srand();
     }
     let mut vec: Vec<i32> = Vec::new();
-    for i in 0..size {
+    for _ in 0..size {
         unsafe {
             vec.push(rand() % 1000);
         }
@@ -89,11 +165,15 @@ fn gen_random_data(size: i32) -> Vec<i32> {
     return vec;
 }
 
-fn test_sort(sort: &Sort, name: &str) {
+fn test_sort(sort: &dyn Sort, name: &str, dec_len: bool) {
     println!("-=| TESTING {} SORT |=-", name);
     let mut vec: Vec<i32> = gen_random_data(20);
     print_data(&vec);
-    sort.sort(&mut vec, 0, 20);
+    if dec_len {
+        sort.sort(&mut vec, 0, 19);
+    } else {
+        sort.sort(&mut vec, 0, 20);
+    }
     print_data(&vec);
 }
 
@@ -104,9 +184,11 @@ extern "C" {
 
 fn main() {
     let sel_sort: SelectionSort = SelectionSort{};
-    test_sort(&sel_sort, "SELECTION");
+    test_sort(&sel_sort, "SELECTION", false);
     let ins_sort: InsertionSort = InsertionSort{};
-    test_sort(&ins_sort, "INSERTION");
-    let merge_sort: MergeSort;
-    let quick_sort: QuickSort;
+    test_sort(&ins_sort, "INSERTION", false);
+    let merge_sort: MergeSort = MergeSort{};
+    test_sort(&merge_sort, "MERGE", true);
+    let quick_sort: QuickSort = QuickSort{};
+    test_sort(&quick_sort, "QUICK", true);
 }
--- sort/swift/Sources/swift/main.swift
+++ sort/swift/Sources/swift/main.swift
@@ -1,21 +1,173 @@
 class Sort {
-    func sort() {}
+    func sort(data: inout [Int], low: Int, high: Int) {}
 }
 
 class SelectionSort: Sort {
-
+    override func sort(data: inout [Int], low: Int, high: Int) {
+        let N = high - low
+        var min = 0
+        for i in 0...N {
+            min = i
+            for j in i...N {
+                if data[j] < data[min] {
+                    min = j
+                }
+            }
+            data.swapAt(i, min)
+        }
+    }
 }
 
 class InsertionSort: Sort {
+    override func sort(data: inout [Int], low: Int, high: Int) {
+        let N = high - low
 
+        for i in 1...N {
+            var j = i - 1
+            let key = data[i]
+            while j >= 0 && data[j] > key {
+                data[j+1] = data[j]
+                j-=1
+            }
+            data[j + 1] = key
+        }
+    }
 }
 
 class MergeSort: Sort {
+    override func sort(data: inout [Int], low: Int, high: Int) {
+        if high <= low {
+            return
+        }
 
+        let mid = low + (high - low) / 2
+        sort(data:&data, low:low, high: mid)
+        sort(data:&data, low:mid+1, high: high)
+        merge(data:&data, low: low, mid: mid, high: high)
+    }
+
+    func merge(data: inout [Int], low: Int, mid: Int, high: Int) {
+	    let leftHalfSize = mid - low + 1
+	    let rightHalfSize = high - mid
+
+        var i = 0
+	    var j = 0
+	    var k = low
+
+        var left = [Int]()
+        for i in 0...leftHalfSize {
+            left.append(data[low + i])
+        }
+        var right = [Int]()
+        for j in 0...rightHalfSize {
+            right.append(data[mid + j + 1])
+        }
+
+        i = 0
+	    j = 0
+
+        while i < leftHalfSize && j < rightHalfSize {
+            if left[i] <= right[j] {
+                data[k] = left[i]
+                i+=1
+            } else {
+                data[k] = right[j]
+                j+=1
+            }
+            k+=1
+        }
+
+        while i < leftHalfSize {
+            data[k] = left[i]
+            i+=1
+            k+=1
+	    }
+
+	    while j < rightHalfSize {
+            data[k] = right[j]
+            j+=1
+            k+=1
+	    }
+    }
 }
 
 class QuickSort: Sort {
+    override func sort(data: inout [Int], low: Int, high: Int) {
+        if high <= low {
+            return
+        }
 
+        let part:Int = partition(data:&data, low:low, high:high)
+        sort(data:&data, low:low, high: part - 1)
+        sort(data:&data, low: part + 1, high: high)
+    }
+
+    func partition(data: inout [Int], low: Int, high: Int) -> Int {
+        var i: Int = low
+        var j: Int = high + 1
+        let tmp = data[low]
+
+        while true {
+            i+=1
+            while data[i] < tmp {
+                i+=1
+                if i == high {
+                    break
+                }
+		    }
+		    j-=1
+		    while tmp < data[j] {
+                j-=1
+                if j == low {
+                    break
+                }
+		    }
+        
+            if i >= j {
+			    break
+            }
+            data.swapAt(i,j)
+        }
+        data.swapAt(low, j)
+        return j
+    }
 }
 
-print("Hello, world!")
+func generateRandomData(size: Int) -> [Int] {
+    var data = [Int]()
+    for _ in 0...size {
+        data.append(Int.random(in: 0 ..< 1000))
+    }
+
+    return data
+}
+
+func printData(data: [Int]) {
+    print(data)
+}
+
+func testSort(sort: Sort, name: String, decLen: Bool) {
+    print("-=| " + name + " SORT |=-")
+    var data = generateRandomData(size:20)
+    printData(data: data)
+    if decLen {
+        sort.sort(data: &data, low:0, high:19)
+    } else {
+        sort.sort(data: &data, low:0, high:20)
+    }
+    
+    printData(data: data)
+}
+
+func main() {
+    let selSort = SelectionSort()
+    testSort(sort: selSort, name:"SELECTION", decLen:false)
+    let insSort = InsertionSort()
+    testSort(sort: insSort, name:"INSERTION", decLen:false)
+    let mergeSort = MergeSort()
+    testSort(sort: mergeSort, name: "MERGE", decLen:true)
+    let quickSort = QuickSort()
+    testSort(sort: quickSort, name: "QUICK", decLen: true)
+}
+
+main()
